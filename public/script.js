1923994f11734843fac3a77e006f2da8d4c447d1
GemTran <162384383+GemTran@users.noreply.github.com>
2024-04-05 19:32:16 +1100

    cnv

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
 const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+cnv.width = innerWidth;
+cnv.height = innerHeight;
 
 // get canvas context
 const ctx = cnv.getContext ('2d')
