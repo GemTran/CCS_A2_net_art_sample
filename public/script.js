721737fb6286f125e5017b85e34b042ebbee7dfc
GemTran <162384383+GemTran@users.noreply.github.com>
2024-04-05 18:33:03 +1100

    script

--- public/script.js
+++ public/script.js
@@ -5,40 +5,47 @@ const cnv = document.getElementById (`cnv_element`)
 cnv.width = innerWidth
 cnv.height = innerHeight
 
-const ctx = cnv.getContext (`2d`)
+// get canvas context
+const ctx = cnv.getContext ('2d')
 
 // these option values give a nice tree, I think
 const options = {
-   mult : {
-       l : 0.6,
-       r : 0.7,
-   },
+    mult : {
+        l : 0.6,
+        r : 0.7,
+    },
 
-   angle : {
-       l : -TAU / 4,
-       r :  TAU / 7,
-   }
+    angle : {
+        l : -TAU / 4,
+        r :  TAU / 7,
+    }
 }
 
-const seed = new Vector(cnv.width / 2, cnv.height);
-const shoot = new Vector (0, -150);
+const seed = new Vector (cnv.width / 2, cnv.height)
+const shoot = new Vector (0, -150)
 
-const tree = new Tree (seed, shoot, 8, ctx)
+// this time constructing an object with the class
+// passing it the same arguments, and also the 
+// canvas context, then storing it in a variable
+const tree = new Tree (seed, shoot, 8, options, ctx)
 
+// function to draw the frames
+// accepts the argument 'now'
+// which requestAnimationFrame will pass
 // the current time to, in milliseconds
 function draw_frame (now) {
 
-   // clear the canvas
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, cnv.width, cnv.height)
+    // clear the canvas
+    ctx.fillStyle = `white`
+    ctx.fillRect (0, 0, cnv.width, cnv.height)
 
-   // convert time to seconds
-   // and pass to .draw method of tree
-   tree.draw (now / 1000)
+    // convert time to seconds
+    // and pass to .draw method of tree
+    tree.draw (now / 1000)
 
-   // wait for the next frame 
-   // then call draw_frame again
-   requestAnimationFrame (draw_frame)
+    // wait for the next frame 
+    // then call draw_frame again
+    requestAnimationFrame (draw_frame)
 }
 
 // initiate the animation
